cd112022e8519cb98955afc05a9c7a69e757e4f0
OwenMaddox <Owen@maddoxbox.com>
2023-04-23 13:49:55 -0500

    Add Kicker With Rail purchase page

--- src/pages/KickerWithRail.js
+++ src/pages/KickerWithRail.js
@@ -5,14 +5,40 @@ function KickerWithRail() {
     <div className="margin size">
       <div className="container text-center">
         <div className="row">
-          <div className="col">
-            <img src={KickerWithRailImg} alt="" />
+          <div className="col imgholder">
+            <img src={KickerWithRailImg} alt="Kicker With Rail" />
           </div>
           <div className="col">
-            <div className="card">
+            <div className="card txt size">
               <div className="card-body">
                 <h1 className="card-title cardtxt">Kicker With Rail</h1>
-                <p className="card-text">$12</p>
+                <p className="card-text">$12 + 12$ Shipping</p>
+                <p>$24 Total</p>
+                <form
+                  target="paypal"
+                  action="https://www.paypal.com/cgi-bin/webscr"
+                  method="post"
+                >
+                  <input type="hidden" name="cmd" value="_s-xclick" />
+                  <input
+                    type="hidden"
+                    name="hosted_button_id"
+                    value="UMXGJJAEDTDNL"
+                  />
+
+                  <button
+                    class=" px-4 bg-blue-600 rounded text-white hover:bg-blue-400"
+                    type="submit"
+                    border="0"
+                    name="submit"
+                    alt="PayPal - The safer, easier way to pay online!"
+                  >
+                    Add To Cart
+                  </button>
+                </form>
+
+                <p className="spacing">8 x 4 inch wood Kicker with a smooth</p>
+                <p>1 inch tall rail on the side</p>
               </div>
             </div>
           </div>
